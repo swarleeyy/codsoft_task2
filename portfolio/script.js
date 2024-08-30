e611103666ebd5929af88719fe1f00152cf357c0
swarleeyy <pillaiaryan8@gmail.com>
2024-08-30 20:40:10 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    new Typed('#type-it', {
+        strings: ['Web-Developer', 'Web-designer','Data Analyst','Photographer','Writer'],
+        typeSpeed: 110,
+        loop:true,
+    });
+});
